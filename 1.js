73643e8a4204d7d88d9b6128df7a8ae603b31b94
mohamedhosny2685 <mohamedhosny2685@gmail.com>
2024-09-12 22:20:47 +0300

    add js

---
+++ 1.js
@@ -0,0 +1,29 @@
+const noteInput = document.getElementById("noteInput");
+var notesArray = [];
+
+
+function addNote() {
+    const newNote = noteInput.value;
+    if (newNote == "") {
+        alert("Please enter valid text");
+    } else {
+        notesArray.push(newNote);
+        noteInput.value = "";
+        saveNotes();
+        console.log(notesArray);
+    }
+}
+
+function saveNotes() {
+    const notesString = JSON.stringify(notesArray);
+    localStorage.setItem("notes", notesString);
+}
+
+function getNotes() {
+    const notesString = localStorage.getItem("notes");
+    if (notesString == null) {
+        console.log("notesString null");
+    } else {
+        notesArray = JSON.parse(notesString);
+    }
+}
