d33619f88e7f1446540f2168a902e6b2e669173a
mohamedhosny2685 <mohamedhosny2685@gmail.com>
2024-09-27 16:53:29 +0300

    Mido

--- 1.js
+++ 1.js
@@ -1,5 +1,35 @@
 const noteInput = document.getElementById("noteInput");
+const notesDiv = document.getElementById("notesDiv");
+const zeroNotes = document.getElementById("zeroNotes");
+const optionsDiv = document.getElementById("optionsDiv");
+const loginDiv = document.getElementById("loginDiv");
+const signupDiv = document.getElementById("signupDiv");
+
 var notesArray = [];
+
+function showOptions() {
+    zeroNotes.style.display = "none";
+    notesDiv.style.display = "none";
+    optionsDiv.style.display = "flex";
+}
+
+function showHome() {
+    loginDiv.style.display = "block";
+    signupDiv.style.display = "none";
+    notesDiv.style.display = "block";
+    optionsDiv.style.display = "none";
+    showNotes();
+}
+
+function showSignUp() {
+    loginDiv.style.display = "none";
+    signupDiv.style.display = "block";
+}
+
+function showLogin() {
+    loginDiv.style.display = "block";
+    signupDiv.style.display = "none";  
+}
 
 
 function addNote() {
@@ -10,6 +40,7 @@ function addNote() {
         notesArray.push(newNote);
         noteInput.value = "";
         saveNotes();
+        showNotes();
         console.log(notesArray);
     }
 }
@@ -27,3 +58,45 @@ function getNotes() {
         notesArray = JSON.parse(notesString);
     }
 }
+
+function showNotes() {
+    
+    getNotes();
+
+    if(notesArray.length > 0) {
+        zeroNotes.style.display = "none";
+    } else {
+        zeroNotes.style.display = "block";
+    }
+
+    notesDiv.innerHTML = "";
+
+    for (let index = 0; index < notesArray.length; index++) {
+        const element = notesArray[index];
+        
+        const newDiv = document.createElement("div");
+
+        const newParagraph = document.createElement("p");
+        newParagraph.innerText = element;
+        newDiv.appendChild(newParagraph);
+
+        const deleteIcon = document.createElement("i");
+        deleteIcon.className = "bi bi-trash3";
+        deleteIcon.onclick = function () {
+
+            if ( confirm("Are you sure to delete note?") == true ) {
+                notesArray.splice(index, 1);
+                saveNotes();
+                showNotes();
+            }
+            
+        }
+        newDiv.appendChild(deleteIcon);
+
+        notesDiv.appendChild(newDiv);
+
+    }
+
+}
+
+showNotes();
